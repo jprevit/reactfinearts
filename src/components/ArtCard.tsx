bdd9d420da06f76586bc6ae23bd67677cfe5d00f
John Previtera <johngprevitera@gmail.com>
2024-12-15 11:34:28 -0700

    conditionally rendering admirers counter

--- src/components/ArtCard.tsx
+++ src/components/ArtCard.tsx
@@ -5,25 +5,37 @@ type ArtCardProps = {
     art: Artwork
 }
 
+
 export default function ArtCard({ art }: ArtCardProps) {
     function setActive() {
         AppState.activeArt = art
     }
 
-    return (
-        <div className="row d-flex align-items-baseline m-3 g-4">
-            <div className="col-12">
-                <img src={art.small} alt={art.description} className="img-fluid artborder" data-bs-toggle="modal" data-bs-target="#artDetailsModal" onClick={setActive} />
-            </div>
+    function hasAdmirers() {
+
+        art.admirers.length
+        return art.admirers.length > 0 ?
             <div className="col-12">
                 <div className="row justify-content-around fs-5 text-uppercase">
-                    <p className="col-2">Admirers:</p>
+                    <div className="col-2">Admirers:</div>
                     <div className="col-2 d-flex">
                         <p className="me-2">{art.admirers.length}</p>
                         <i className="mdi mdi-heart"></i>
                     </div>
                 </div>
             </div>
+            :
+            <></>
+    }
+
+    const admirers = hasAdmirers();
+
+    return (
+        <div className="row d-flex align-items-baseline m-3 g-4">
+            <div className="col-12">
+                <img src={art.small} alt={art.description} className="img-fluid artborder" data-bs-toggle="modal" data-bs-target="#artDetailsModal" onClick={setActive} loading="lazy" />
+            </div>
+            {admirers}
         </div >
     )
 }
