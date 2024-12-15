c9e9121a2109bf559dd70e051e092aa12477fe93
John Previtera <johngprevitera@gmail.com>
2024-12-15 08:23:26 -0700

    added admirers

--- src/components/ArtCard.tsx
+++ src/components/ArtCard.tsx
@@ -11,9 +11,18 @@ export default function ArtCard({ art }: ArtCardProps) {
     }
 
     return (
-        <div className="row d-flex justify-content-center m-3 g-4">
+        <div className="row d-flex align-items-baseline m-3 g-4">
             <div className="col-12">
                 <img src={art.small} alt={art.description} className="img-fluid artborder" data-bs-toggle="modal" data-bs-target="#artDetailsModal" onClick={setActive} />
+            </div>
+            <div className="col-12">
+                <div className="row justify-content-around fs-5 text-uppercase">
+                    <p className="col-2">Admirers:</p>
+                    <div className="col-2 d-flex">
+                        <p className="me-2">{art.admirers.length}</p>
+                        <i className="mdi mdi-heart"></i>
+                    </div>
+                </div>
             </div>
         </div >
     )
